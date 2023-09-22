36e38441eff59ed1210dfa4bc429841cb98656d9
Mina <64227723+Mina314@users.noreply.github.com>
2023-09-21 22:50:59 -0700

    Update project.sql

--- project.sql
+++ project.sql
@@ -1,62 +1,61 @@
--- Task: Create a list that shows the "average customer lifetime value" grouped by the different districts.
--- Example:
--- If there are two customers in "District 1" where one customer has a total (lifetime) spent of $1000 and 
--- the second customer has a total spent of $2000 then the "average customer lifetime spent" in this district is $1500.
--- So, first, you need to calculate the total per customer and then the average of these totals per district.
--- Question: Which district has the highest average customer lifetime value?
--- Answer: Saint-Denis with an average customer lifetime value of 216.54.
+/*
+Objectives
+Come up with flu shots dashboard for 2022 that does the following
+
+1.) Total % of patients getting flu shots (patient is 6 months or older) stratified by
+   a.) Age
+   b.) Race
+   c.) County (On a Map)
+   d.) Overall
+2.) Running Total of Flu Shots over the course of 2022
+3.) Total number of Flu shots given in 2022
+4.) A list of Patients that show whether or not they received the flu shots
+   
+Requirements:
+
+Patients must have been "Active at our hospital"
+*/
+
+with active_patients as
+(
+	SELECT DISTINCT patient
+	FROM encounters e
+	JOIN patients p
+	ON e.patient = p.id
+	WHERE start between '2022-01-01' and '2023-01-01'
+	AND p.deathdate is null
+	AND extract(month from age('2022-12-31', p.birthdate)) >= 6
+),
+
+flu_shot_2022 as
+(
+	SELECT
+	patient,
+	min(date) AS first_flu_shot_2022
+	FROM immunizations
+	WHERE code = '5302'
+	AND date between '2022-01-01' and '2023-01-01'
+	GROUP BY patient
+	ORDER by first_flu_shot_2022
+)
 
 SELECT
-district,
-ROUND(AVG(total),2)
-FROM (SELECT
-p.customer_id,
-district,
-SUM(amount) AS total
-FROM customer c
-JOIN payment p
-ON p.customer_id = c.customer_id
-JOIN address a
-ON a.address_id = c.address_id 
-GROUP BY p.customer_id, district) AS sub
-GROUP BY district
-ORDER by AVG(total) DESC
-
--- Task: Create a list that shows all payments including the payment_id, amount, and the film category (name) plus 
--- the total amount that was made in this category. 
--- Order the results ascendingly by the category (name) and as second order criterion by the payment_id ascendingly.
--- Question: What is the total revenue of the category 'Action' and what is the lowest payment_id in that category 'Action'?
--- Answer: Total revenue in the category 'Action' is 4375.85 and the lowest payment_id in that category is 16055.
-
-SELECT
-title,
-payment_id,
-amount,
-name,
-(SELECT SUM(amount) 
-FROM payment p
-LEFT JOIN rental r
-ON p.rental_id = r.rental_id
-LEFT JOIN inventory i
-ON i.inventory_id = r.inventory_id
-LEFT JOIN film f
-ON f.film_id = i.film_id
-LEFT JOIN film_category fc
-ON fc.film_id = i.film_id
-LEFT JOIN category c2
-ON c2.category_id = fc.category_id
-WHERE c.name = c2.name)
-
-FROM payment p
-LEFT JOIN rental r
-ON p.rental_id = r.rental_id
-LEFT JOIN inventory i
-ON i.inventory_id = r.inventory_id
-LEFT JOIN film f
-ON f.film_id = i.film_id
-LEFT JOIN film_category fc
-ON fc.film_id = i.film_id
-LEFT JOIN category c
-ON c.category_id = fc.category_id
-ORDER BY name
-
+	id,
+	first,
+	last,
+	birthdate,
+	extract(year from age('2022-12-31', birthdate)) AS age,
+	race,
+	gender,
+	ethnicity,
+	county,
+	zip,
+	f.first_flu_shot_2022,
+	CASE WHEN f.patient is not null THEN 1
+	ELSE 0
+	END as flu_shot_2022
+FROM 
+patients p
+LEFT JOIN flu_shot_2022 f
+ON p.id = f.patient
+WHERE p.id IN (SELECT patient FROM active_patients)
